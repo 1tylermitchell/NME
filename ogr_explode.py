39122e5299ad2da0808ed46ce986ebd21efa8844
Tyler Mitchell <tylermitchell@shaw.ca>
2013-01-31 21:34:57 -0800

    added ability to specify optional zfield for third dimension in output

--- ogr_explode.py
+++ ogr_explode.py
@@ -24,15 +24,16 @@ def startup(argv):
   input = argv[1]
   output = argv[2]
   outfile = initialise_output(output)
+  zfield = check_zfield(argv)
   instatus = check_input(input)
   if instatus == True:
     ds = ogr.Open(input)
     lay = ds.GetLayer(0)
     fcnt,rcnt,pcnt=0,0,0
-    return input,output,outfile,ds,lay,fcnt,rcnt,pcnt,instatus
+    return input,output,outfile,ds,lay,fcnt,rcnt,pcnt,instatus,zfield
   else:
     print "Invalid Input Datasource"
-    return input,output,outfile,False,False,0,0,0,instatus
+    return input,output,outfile,False,False,0,0,0,instatus,zfield
 
 def syntax_check(argv):
   if len(argv) < 3:
@@ -43,10 +44,10 @@ def print_usage():
   print """
   Syntax:
      
-    ogr_explode.py <input datasource> <output filename>
+    ogr_explode.py <input datasource> <output filename> [zfield name]
   
-  Take Z value from point features if they have one, I need
-  to add ability to specify a field to take them from.
+  Takes Z value from point features if they have one, or optionally from
+  a specified field name.  Order matters :) 
   """
   return
    
@@ -58,6 +59,13 @@ def check_input(input):
     return True
   except (AttributeError):
     return False
+
+def check_zfield(argv):
+  if len(argv) > 2:
+    zfield = argv[3]
+    return zfield
+  else:
+    return None
 
 def initialise_output(output):
   outfile = open(output,'w')
@@ -90,37 +98,50 @@ def check_ftype(lay):
  
   return lay_geom
 
-def process_lines(geom,pcnt):
- points = geom.GetPointCount()
- for p in xrange(points):
-   pcnt+=1
-   lon, lat, z = geom.GetPoint(p)
-   outstr = "%s,%s,%s\n" % (lon,lat,z)
-   write_output(outfile,outstr) 
- return pcnt
+def process_lines(geom,pcnt,zfield):
+  points = geom.GetPointCount()
+  for p in xrange(points):
+    pcnt+=1
+    lon, lat, z = geom.GetPoint(p)
+    zval = get_zval(feat,zfield)
+    if zval != None:
+      z = zval
+    outstr = "%s,%s,%s\n" % (lon,lat,z)
+    write_output(outfile,outstr) 
+  return pcnt
 
-def process_polygons(geom,rcnt,pcnt):
+def process_polygons(geom,rcnt,pcnt,zfield):
   for ring in geom:
     rcnt+=1
     points = ring.GetPointCount()
     for p in xrange(points):
       pcnt+=1
       lon, lat, z = ring.GetPoint(p)
+      zval = get_zval(feat,zfield)
+      if zval != None:
+        z = zval
       outstr = "%s,%s,%s\n" % (lon,lat,z)
       write_output(outfile,outstr)
   return rcnt,pcnt
 
+def get_zval(feat,zfield):
+  if zfield in feat.keys():
+    zfield_val = feat.GetFieldAsString(zfield)
+  else:
+    zfield_val = None
+  return zfield_val
+
 if __name__ == '__main__':
-  input,output,outfile,ds,lay,fcnt,rcnt,pcnt,instatus = startup(argv)
+  input,output,outfile,ds,lay,fcnt,rcnt,pcnt,instatus,zfield = startup(argv)
   if instatus == True:
     for feat in lay:
       fcnt+=1
       geom = feat.GetGeometryRef()
       lay_geom = check_ftype(lay)
-      if lay_geom == 'LINESTRING':
-        pcnt = process_lines(geom,pcnt)
+      if lay_geom == 'LINESTRING' or lay_geom == 'POINT':
+        pcnt = process_lines(geom,pcnt,zfield)
       elif lay_geom == 'POLYGON':
-        rcnt,pcnt = process_polygons(geom,rcnt,pcnt)
+        rcnt,pcnt = process_polygons(geom,rcnt,pcnt,zfield)
       else:
         print "Unexpected Input Geometry Type"
         print "Script expects Linestring or Polygon data"
